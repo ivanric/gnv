4dfe4a9503df93e6e70eee4c1591bfcf6170f667
gnv <ivanric12@gmail.com>
2017-01-20 18:29:42 -0400

    20-01-2017 (se modifico index.jsp)

--- configuraciones/express.js
+++ configuraciones/express.js
@@ -1,13 +1,17 @@
 var express=require('express');
+var path = require('path');
 //var rutas=require('../aplicasion/rutas/')
 var rutas='../aplicasion/rutas/';
 var app=express();
-//var http=require('http');
+                                                                                                                                                                                                                                                                                //var http=require('http');
 //var servidor=http.createServer(app);
 
 //app.use('/',rutas)(app);
 app.set('views', './aplicasion/vistas');
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, '../aplicasion/publico')));
 
 require(rutas+'inicio.js')(app);
 module.exports=function(){
